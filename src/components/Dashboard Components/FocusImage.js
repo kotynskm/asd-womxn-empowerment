9fcf94063812ac45fc5fd717148ca8f8d094dffa
EdnaK18 <reginakwanrx@gmail.com>
2022-11-17 17:45:49 -0500

    add focus img compnent with a new random img

---
+++ src/components/Dashboard Components/FocusImage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FocusImage = () => {
+  return (
+    <div id="focusImg">
+      <img src="https://picsum.photos/800/300" />
+    </div>
+  );
+}
+
+export default FocusImage;
