6494eede0046d960ea82d422ed9f9374244646a4
EdnaK18 <reginakwanrx@gmail.com>
2022-11-18 12:24:18 -0500

    cleaned up tree and dependencies

--- controllers/authController.js
+++
@@ -1,5 +0,0 @@
-const User = require("../src/server/models/userModel");
-
-exports.login = async (req, res, next) => {
-  // the login function
-};
--- src/server/server.js
+++ src/server/server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use('/login', authRouter, )
+app.use('/login', authRouter)
 
 
 
