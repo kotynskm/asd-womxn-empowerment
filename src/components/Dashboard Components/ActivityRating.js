3f691d0670035a396081e8693d9103e83f0260a3
ecraw24 <elcrawford24@gmail.com>
2022-11-17 18:16:52 -0600

    Added tabs for daily activity that can pull from database

--- src/components/Dashboard Components/ActivityRating.js
+++ src/components/Dashboard Components/ActivityRating.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Tabs from './Tabs';
+import './Tab.css' 
 
 const ActivityRating = () => {
   
@@ -20,57 +22,83 @@ const ActivityRating = () => {
     };
 
   // Prep list of howTos based on comma seperated list in DB
-  const howTos = activities.howto.map((activity) => <li>{activity}</li>);
+  const howTos = activities.howto.map((listItem) => <li>{listItem}</li>);
+  const type = activities.type; 
+
+  // function loadTabs(props) {
+  //   const tabDiv = activities.map(
+  //     (activity) =>
+  //       (<div label={activity.type}>
+  //         <div id='activityInfoContainer' >
+  //           <h3 id='activityTitle'>{activity.title}</h3>
+  //           <p id='activityDescription'>{activity.description}</p>
+  //           <ol id='activityHowTo'>{activity.howto}</ol>
+  //           <a id = 'activityLink' href={activity.link}><cite id="activityCitation">{activity.citation}</cite></a>
+  //         </div>
+  //         <form>{loadForm()}</form>
+  //       </div>)
+  //   ); 
+  //   const completedTabs = (<Tabs> {tabDiv} </Tabs>); 
+  //   return completedTabs; 
+  // }
+
+  function loadForm(props) {
+    return (
+      <div>
+        <br></br>
+        <input id='activityCompleted' type="checkbox"></input>
+        <label>Completed</label>
+        <br></br>
+        <br></br>
+        <label>
+          How did this activity make you feel?
+          <br></br>
+          <select id='activityRatingSelection'>
+            <option>Excited</option>
+            <option>Happy</option>
+            <option>Relaxed</option>
+            <option>Sad</option>
+            <option>Frustrated</option>
+            <option>Anxious</option>
+            <option>Other</option>
+          </select>
+        </label>
+        <div>
+          <br></br>
+          <label>Did this activity positively contribute to your personal growth or sense of wellbeing?</label>
+          <br></br>
+          <input type="radio" id="yes" name="wellbeingRadio" value='yes'/>
+          <label for="yes">Yes</label>
+          <input type="radio" id="no" name="wellbeingRadio" value='no'/>
+          <label for="no">No</label>
+        </div>
+        <br></br>
+        <label>
+          Activity Journal Entry
+          <br></br>
+          <textarea id="activityNotes" name="activityNotes" rows="4" cols="30"></textarea>
+        </label>
+        <br></br>
+        <button type="submit" value="activitySubmit">Submit Activity</button>
+        <button type="reset" value="activityReset">Reset Activity</button>
+      </div>
+    )
+  }
 
   return (
     <>
-      {/* <div id="activityRating">ActivityRating.js</div> */}
-        <div id='activityInfoContainer'>
-          <h3 id='activityTitle'>{activities.title}</h3>
-          <p id='activityDescription'>{activities.description}</p>
-          <ol id='activityHowTo'>{howTos}</ol>
-          <a id = 'activityLink' href={activities.link}><cite id="activityCitation">{activities.citation}</cite></a>
-        </div>
-        <form>
-          <div>
-            <br></br>
-            <input id='activityCompleted' type="checkbox"></input>
-            <label>Completed</label>
-            <br></br>
-            <br></br>
-            <label>
-              How did this activity make you feel?
-              <br></br>
-              <select id='activityRatingSelection'>
-                <option>Excited</option>
-                <option>Happy</option>
-                <option>Relaxed</option>
-                <option>Sad</option>
-                <option>Frustrated</option>
-                <option>Anxious</option>
-                <option>Other</option>
-              </select>
-            </label>
-            <div>
-              <br></br>
-              <label>Did this activity positively contribute to your personal growth or sense of wellbeing?</label>
-              <br></br>
-              <input type="radio" id="yes" name="wellbeingRadio" value='yes'/>
-              <label for="yes">Yes</label>
-              <input type="radio" id="no" name="wellbeingRadio" value='no'/>
-              <label for="no">No</label>
-            </div>
-            <br></br>
-            <label>
-              Activity Journal Entry
-              <br></br>
-              <textarea id="activityNotes" name="activityNotes" rows="4" cols="30"></textarea>
-            </label>
-            <br></br>
-            <button type="submit" value="activitySubmit">Submit Activity</button>
-            <button type="reset" value="activityReset">Reset Activity</button>
+      <Tabs>
+        <div label={type}>
+          <div id='activityInfoContainer' >
+            <h3 id='activityTitle'>{activities.title}</h3>
+            <p id='activityDescription'>{activities.description}</p>
+            <ol id='activityHowTo'>{howTos}</ol>
+            <a id = 'activityLink' href={activities.link}><cite id="activityCitation">{activities.citation}</cite></a>
           </div>
-        </form>
+          <form>{loadForm()}</form>
+        </div>
+        <div label = "Example Tab"></div>
+      </Tabs>
     </>
   );
 };
